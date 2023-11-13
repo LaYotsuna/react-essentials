631774b044b2e2451697c8355b5e680852763d0a
Larissa Cerqueira <larissacerqueirayt@gmail.com>
2023-11-13 11:54:13 -0300

    Created a handle click func

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,8 +1,12 @@
 export default function TabButton(props) {
+  function handleClick() {
+    console.log("Hello World");
+  }
+
   return (
     <li>
       {/* Children refers to the content between the button tag in the App file */}
-      <button>{props.children}</button>
+      <button onClick={handleClick}>{props.children}</button>
     </li>
   );
 }
